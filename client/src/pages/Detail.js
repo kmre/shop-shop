eec86616ab74e5c1f2cb562b8e56a829aede5be9
kmre <karla.romero7@gmail.com>
2021-10-22 00:02:04 -0400

    22.1.6 finished

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,50 +1,80 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import React, {useEffect, useState} from 'react';
+import {Link, useParams} from 'react-router-dom';
+import {useQuery} from '@apollo/client';
 
-import { QUERY_PRODUCTS } from '../utils/queries';
+import {QUERY_PRODUCTS} from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 
+import {useStoreContext} from "../utils/GlobalState";
+import {UPDATE_PRODUCTS} from "../utils/actions";
+
 function Detail() {
-  const { id } = useParams();
+    // const { id } = useParams();
 
-  const [currentProduct, setCurrentProduct] = useState({});
+    // const [currentProduct, setCurrentProduct] = useState({});
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+    // const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+    // const products = data?.products || [];
 
-  useEffect(() => {
-    if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
+    // useEffect(() => {
+    // if (products.length) {
+    //     setCurrentProduct(products.find((product) => product._id === id));
+    // }
+    // }, [products, id]);
+
+    const [state, dispatch] = useStoreContext();
+    const {id} = useParams();
+
+    const [currentProduct, setCurrentProduct] = useState({})
+
+    const {loading, data} = useQuery(QUERY_PRODUCTS);
+
+    const {products} = state;
+
+    useEffect(() => {
+        if (products.length) {
+            setCurrentProduct(products.find(product => product._id === id));
+        } else if (data) {
+            dispatch({type: UPDATE_PRODUCTS, products: data.products});
+        }
+    }, [products, data, dispatch, id]);
+
+    return (<> {
+        currentProduct ? (<div className="container my-1">
+            <Link to="/">← Back to Products</Link>
+
+            <h2> {
+                currentProduct.name
+            }</h2>
+
+            <p> {
+                currentProduct.description
+            }</p>
+
+            <p>
+                <strong>Price:</strong>${
+                currentProduct.price
+            }
+                {' '}
+                <button>Add to Cart</button>
+                <button>Remove from Cart</button>
+            </p>
+
+            <img src={
+                    `/images/${
+                        currentProduct.image
+                    }`
+                }
+                alt={
+                    currentProduct.name
+                }/>
+        </div>) : null
     }
-  }, [products, id]);
-
-  return (
-    <>
-      {currentProduct ? (
-        <div className="container my-1">
-          <Link to="/">← Back to Products</Link>
-
-          <h2>{currentProduct.name}</h2>
-
-          <p>{currentProduct.description}</p>
-
-          <p>
-            <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
-            <button>Remove from Cart</button>
-          </p>
-
-          <img
-            src={`/images/${currentProduct.image}`}
-            alt={currentProduct.name}
-          />
-        </div>
-      ) : null}
-      {loading ? <img src={spinner} alt="loading" /> : null}
-    </>
-  );
+        {
+        loading ? <img src={spinner}
+            alt="loading"/> : null
+    } </>);
 }
 
 export default Detail;
