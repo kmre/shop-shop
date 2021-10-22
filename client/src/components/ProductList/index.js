74896d2bcf70515cec9216b132f56b164f359557
kmre <karla.romero7@gmail.com>
2021-10-21 23:51:39 -0400

    22.1.6 products

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,47 +1,67 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
+import React, {useEffect} from 'react';
+import {useQuery} from '@apollo/client';
+import {useStoreContext} from '../../utils/GlobalState';
+import {UPDATE_PRODUCTS} from '../../utils/actions';
 
 import ProductItem from '../ProductItem';
-import { QUERY_PRODUCTS } from '../../utils/queries';
+import {QUERY_PRODUCTS} from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
 
-function ProductList({ currentCategory }) {
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+function ProductList() {
+    // const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+    // const products = data?.products || [];
 
-  function filterProducts() {
-    if (!currentCategory) {
-      return products;
+    // function filterProducts() {
+    // if (!currentCategory) {
+    //     return products;
+    // }
+
+    // return products.filter(
+    //     (product) => product.category._id === currentCategory
+    // );
+    // };
+
+    const [state, dispatch] = useStoreContext();
+
+    const {currentCategory} = state;
+
+    const {loading, data} = useQuery(QUERY_PRODUCTS);
+
+    useEffect(() => {
+        if (data) {
+            dispatch({type: UPDATE_PRODUCTS, products: data.products});
+        }
+    }, [data, dispatch]);
+
+    function filterProducts() {
+        if (!currentCategory) {
+            return state.products;
+        }
+        return state.products.filter(product => product.category._id === currentCategory);
     }
 
-    return products.filter(
-      (product) => product.category._id === currentCategory
+    return (<div className="my-2">
+        <h2>Our Products:</h2>
+        {state.products.length ? (
+                    <div className="flex-row">
+                      {filterProducts().map((product) => (
+                        <ProductItem
+                          key={product._id}
+                          _id={product._id}
+                          image={product.image}
+                          name={product.name}
+                          price={product.price}
+                          quantity={product.quantity}
+                        />
+                      ))}
+                    </div>
+                  ) : (
+                    <h3>You haven't added any products yet!</h3>
+                  )}
+                  {loading ? <img src={spinner} alt="loading" /> : null}
+                </div>
     );
-  }
+    }
 
-  return (
-    <div className="my-2">
-      <h2>Our Products:</h2>
-      {products.length ? (
-        <div className="flex-row">
-          {filterProducts().map((product) => (
-            <ProductItem
-              key={product._id}
-              _id={product._id}
-              image={product.image}
-              name={product.name}
-              price={product.price}
-              quantity={product.quantity}
-            />
-          ))}
-        </div>
-      ) : (
-        <h3>You haven't added any products yet!</h3>
-      )}
-      {loading ? <img src={spinner} alt="loading" /> : null}
-    </div>
-  );
-}
-
-export default ProductList;
+    export default ProductList;
